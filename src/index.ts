21e89dfeb3e1312ccb086871f88bd114c31d1953
lgotarra <97gotarra@gmail.com>
2021-06-26 10:53:03 +0200

    Added Cors middleware.

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,38 @@
 import "./helpers/env";
 import express from "express";
 import http from "http";
+import cors from "cors";
 
 const SERVER_PORT = process.env.SERVER_PORT;
 
 const app = express();
+
+// ------------------------------------------------------------------------------------------------------------
 
 /*
  *
  * Middleware
  *
  */
+
 // Parses Content-Type payload to JSON.
 app.use(express.json());
 
+// Allow cross-origin request
+app.use(cors());
+
+// ------------------------------------------------------------------------------------------------------------
+
+// Default Endpoint
 app.get("/", async (req, res) => {
   res.send("<h1>Welcome to Movielist server</h1>");
 });
 
-/*
- *
- * API Router
- *
- */
-// app.use("/api", require("./api"));
+// ------------------------------------------------------------------------------------------------------------
+
+//TODO: Add Routers
+
+// ------------------------------------------------------------------------------------------------------------
 
 // Server launch
 http.createServer(app).listen(SERVER_PORT, () => {
