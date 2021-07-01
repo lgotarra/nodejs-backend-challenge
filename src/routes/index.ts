b236c399cce5bb488b44c6e68bc9a4f542c7e34a
lgotarra <97gotarra@gmail.com>
2021-07-01 17:55:19 +0200

    added lists router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import usersRouter from "./user/user.routes";
+import listsRouter from "./list/list.routes";
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/lists', listsRouter);
 
 export default routes;
